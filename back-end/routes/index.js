80610bd69c193b4cd61829185178b82d6afd597c
swuleung <sammiwuleung@gmail.com>
2020-02-17 00:51:33 -0500

    Sequelize Base with connection to MySQL

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET home page. */ 
+router.get('/', function(req, res) {
+    models.User.findAll({
+      include: [ models.Task ]
+    }).then(function(users) {
+      res.send({
+        title: 'Sequelize: Express Example',
+        users: users
+      });
+    });
+  });
+
+  router.post('/create', function(req, res) {
+    models.User.create({
+      username: req.body.username
+    }).then(function() {
+      res.redirect('/');
+    });
+  });
+  
+  router.get('/:user_id/destroy', function(req, res) {
+    models.User.destroy({
+      where: {
+        id: req.params.user_id
+      }
+    }).then(function() {
+      res.redirect('/');
+    });
+  });
+  
+  router.post('/:user_id/tasks/create', function (req, res) {
+    models.Task.create({
+      title: req.body.title,
+      UserId: req.params.user_id
+    }).then(function() {
+      res.redirect('/');
+    });
+  });
+  
+  router.get('/:user_id/tasks/:task_id/destroy', function (req, res) {
+    models.Task.destroy({
+      where: {
+        id: req.params.task_id
+      }
+    }).then(function() {
+      res.redirect('/');
+    });
+  });
 
 module.exports = router;
