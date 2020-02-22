b732d88f9e86f01bbbef4cb23b399cb3ea8a9d57
swuleung <sammiwuleung@gmail.com>
2020-02-22 18:18:35 -0500

    Add in the home page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './theme.scss';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home/Home.js';
 
 function App() {
+    return (
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                    <Navbar bg="primary">
+                        <Navbar.Brand id="logo" as={Link} to="/">C4Me</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/search-colleges">Search Colleges</Nav.Link>
+                            <Nav.Link as={Link} to="/find-similar-hs">Find Similar High Schools</Nav.Link>
+                        </Nav>
+                        <Nav className="ml-auto">
+                            <NavDropdown title="John Doe" alignRight id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#tbd">View Profile</NavDropdown.Item>
+                                <NavDropdown.Item href="#tbd">Settings</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#tbd">Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar>
 
-const [apiResponse, setApiResponse] = useState(0);
-
-const callAPI = function()  {
-    console.log('yo');
-
-fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => setApiResponse(res));
-
-}
-
-useEffect(() => {
-    callAPI();
-});
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-<p>{apiResponse}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route render={function () {
+                            return <p>404 Not found</p>
+                        }} />
+                    </Switch>
+                </header>
+            </div>
+        </Router >
+    );
 }
 
 export default App;
---
+++ front-end/src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Home.scss';
+
+const home = () => {
+    return (
+        <div id="home-page">
+            <span id="home-logo">C4Me</span>
+            Search for your dream college
+        </div>
+    );
+}
+
+export default home;
