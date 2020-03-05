f0ea2325698b2ea48dcde88480ef6cbd8b4f59bf
swuleung <sammiwuleung@gmail.com>
2020-03-05 17:53:21 -0500

    Be able to create user

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/createStudent', function (req, res) {
+    models.User.create({
+        username: req.body.username,
+        password: req.body.password
+    }).then((user) => {
+        models.Student.create({
+            username: user.username,
+        })
+    }).then(function () {
+        res.redirect('/');
+    });
+});
+
+router.post('/login', function (req, res) {
+
 });
 
 module.exports = router;
