e8002cbe223404f9ddcc95adf591f4edec28e5ac
Zachary <zacharyzysberg1@gmail.com>
2020-03-22 21:18:34 -0400

    undid test, fixed github email

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -73,7 +73,7 @@ exports.login = async (loginUser) => {
             limit: 1,
             raw: true,
             where: {
-                username: usernam
+                username: username
             }
         });
     } catch (error) {
