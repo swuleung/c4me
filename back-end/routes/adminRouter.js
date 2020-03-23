a9c43b094d5d6cdcd66d9f9b4e5df4b30a6c3776
Jackie Quach <jackiequach14@gmail.com>
2020-03-23 01:18:15 -0400

    Add authentication to scrape routes

--- back-end/routes/adminRouter.js
+++ back-end/routes/adminRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authentication = require('../utils/auth');
 const adminController = require('../controllers/adminController');
 
-router.get('/:username', async function (req, res) {
+router.get('/scrapeCollegeRanking', async function(req, res) {
     if (!req.cookies.access_token) {
         res.status(400).send({ status: "error", error: "No token provided" });
     } else {
@@ -11,8 +11,10 @@ router.get('/:username', async function (req, res) {
         if (!authorized.username) {
             res.clearCookie("access_token");
             res.status(400).send(authorized);
-        } else {
-            let result = await adminController.getAdmin(req.params.username);
+        } else if(!adminController.checkAdmin(authorized.username)) { 
+            res.status(400).send(authorized);
+        } else {    
+            let result = await adminController.scrapeCollegeRankings();
             if (result.error) {
                 if (result.error == 'Something went wrong') res.status(500);
                 else res.status(400);
@@ -22,22 +24,25 @@ router.get('/:username', async function (req, res) {
     }
 });
 
-router.get('/:username/scrapeCollegeRanking', async function(req, res) {
-    let result = await adminController.scrapeCollegeRankings();
-    if(result.error) {
-        if(result.error == 'Something went wrong') res.status(500);
-        else res.status(400);
+router.get('/scrapeCollegeData', async function(req, res) {
+    if (!req.cookies.access_token) {
+        res.status(400).send({ status: "error", error: "No token provided" });
+    } else {
+        let authorized = await authentication.validateJWT(req.cookies.access_token);
+        if (!authorized.username) {
+            res.clearCookie("access_token");
+            res.status(400).send(authorized);
+        } else if(!adminController.checkAdmin(authorized.username)) { 
+            res.status(400).send(authorized);
+        } else {
+            let result = await adminController.scrapeCollegeData();
+            if (result.error) {
+                if (result.error == 'Something went wrong') res.status(500);
+                else res.status(400);
+            }
+            res.send(result);
+        }
     }
-    res.send(result);
-});
-
-router.get('/:username/scrapeCollegeData', async function(req, res) {
-    let result = await adminController.scrapeCollegeData();
-    if(result.error) {
-        if(result.error == 'Something went wrong') res.status(500);
-        else res.status(400);
-    }
-    res.send(result);
 });
 
 module.exports = router;
