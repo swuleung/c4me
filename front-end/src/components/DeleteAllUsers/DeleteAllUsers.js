a7ba66e7bdb3e48e33dce8ae064552a617c6ca1e
Zachary <zacharyzysberg1@gmail.com>
2020-03-22 23:59:36 -0400

    How do I get the front end to recognize I'm an admin; how do I communicate with the adminController to trigger deleteAllUsers?

---
+++ front-end/src/components/DeleteAllUsers/DeleteAllUsers.js
@@ -0,0 +1,11 @@
+import React, { useState } from '../../../node_modules/react';
+import { Link } from '../../../node_modules/react-router-dom';
+import { Form, Button, Alert } from '../../../node_modules/react-bootstrap';
+import './DeleteAllUsers.scss';
+
+
+const deleteEveryStudent =  (props) => {
+};
+
+
+export default deleteEveryStudent;
