4ac6941f9627c37d6afe5e31e5be1d301842532f
swuleung <sammiwuleung@gmail.com>
2020-03-23 23:49:37 -0400

    Comment on Application

--- back-end/models/application.js
+++ back-end/models/application.js
@@ -1,5 +1,8 @@
 'use strict';
-
+/** 
+ * This a JOIN between College & User 
+ * The associations are defined in college.js & user.js
+ */
 module.exports = (sequelize, DataTypes) => {
     let Application = sequelize.define('Application',{
         status: {
