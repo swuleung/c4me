3277e1da5762f084e0fd90f77ae401fda54cec3e
Sammi Wu Leung <sammiwuleung@gmail.com>
2020-03-26 18:08:36 -0400

    View & Edit Student Profile (#4)
    * View Student Profile
    
    * React bootrap items
    
    * Edit Profile front-end load
    
    * Edit Profile back-end & front-end
    
    * Change the workings of applications + authentication fixes
    
    * Comment on Application
    
    * Add & update applications (front & back)
    
    * Fixed loading and redirecting when trying to edit another user
    
    * On create, go to login page
    
    * Fix edit profile button
    
    * Fix minor errors in profile page & updating
    
    * Change const to let
    
    * Remove loaded & filter existing applications
    
    * toString all values in CollegeDropdown
    
    * Added padding to bottom of app & null fix in CollegeDropdown
    
    * Null issue in applications

--- back-end/models/college.js
+++ back-end/models/college.js
@@ -99,6 +99,7 @@ module.exports = (sequelize, DataTypes) => {
         ]
     });
     College.associate = (models) => {
+        College.belongsToMany(models.User, {through: 'Application',  foreignKey: 'college'});
         College.belongsToMany(models.Major, { through: 'CollegeMajors', onDelete: 'CASCADE' });
     }
     return College;
---
+++ front-end/src/components/EditProfile/CollegeDropdown.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Alert, Form } from 'react-bootstrap';
+import './EditProfile.scss';
+import { getAllColleges } from '../../services/api/college';
+
+const CollegeDropdown = (props) => {
+    const [colleges, setColleges] = useState([]);
+    const [errorAlert, setErrorAlert] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const getCollegeOptions = () => {
+        let options = []
+        for (let i = 0; i < colleges.length; i++) {
+            let value = props.selectedValue ? props.selectedValue.toString() : '';
+            if (value === colleges[i].CollegeId.toString()
+                || !props.applications.some(app => (app.college ? app.college.toString() : '') === colleges[i].CollegeId.toString())
+            ) {
+                options.push(
+                    <option key={`${i}-${colleges[i].Name}`} value={colleges[i].CollegeId}>{colleges[i].Name}</option>
+                )
+            }
+        }
+        return options;
+    }
+    useEffect(() => {
+        getAllColleges().then((result) => {
+            if (result.error) {
+                setErrorAlert(true);
+                setErrorMessage(result.error);
+            }
+            if (result.ok) {
+                setErrorAlert(false);
+                setColleges(result.colleges);
+            }
+        });
+    }, [])
+    return (
+        <div>
+            {errorAlert
+                ? <Alert variant="danger">{errorMessage}</Alert>
+                : <Form.Control as="select" index={props.index} value={props.selectedValue ? props.selectedValue : '-1'} onChange={(e) => props.onChange(e)}>
+                    <option value='-1' disabled>Select a college</option>
+                    {getCollegeOptions()}
+                </Form.Control>
+            }
+        </div >
+    );
+}
+
+export default CollegeDropdown;
