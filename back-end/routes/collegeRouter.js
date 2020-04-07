55ecab8321791eee84b55cd4e49c63b171446c2b
Sammi Wu Leung <sammiwuleung@gmail.com>
2020-04-06 20:48:27 -0400

    Applications tracker back-end (#16)
    Needs to be verified

--- back-end/routes/collegeRouter.js
+++ back-end/routes/collegeRouter.js
@@ -72,4 +72,21 @@ router.get('/id/:collegeID/majors', async (req, res) => {
         }
     }
 });
+
+router.post('/id/:collegeID/applications', async (req,res) => {
+    if (!req.cookies.access_token) {
+        res.status(400).send({ status: 'error', error: 'No token provided' });
+    } else {
+        const authorized = await authentication.validateJWT(req.cookies.access_token);
+        if (!authorized.username) {
+            res.clearCookie('access_token');
+            res.status(400).send(authorized);
+        } else {
+            let result = {};
+            result = await collegeController.getApplicationsByCollegeID(req.params.collegeID, req.body.filters);
+            if (result.error) res.status(400);
+            res.send(result);
+        }
+    }
+});
 module.exports = router;
