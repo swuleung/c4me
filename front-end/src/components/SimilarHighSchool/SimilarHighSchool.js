c93ed629ebdf501fcb0873796f11a90d428049cd
swuleung <sammiwuleung@gmail.com>
2020-04-26 18:14:13 -0400

    Add spinners

--- front-end/src/components/SimilarHighSchool/SimilarHighSchool.js
+++ front-end/src/components/SimilarHighSchool/SimilarHighSchool.js
@@ -3,7 +3,7 @@
  */
 import React, { useState, useEffect } from 'react';
 import {
-    Alert, Col, Row, Container,
+    Alert, Col, Row, Container, Spinner,
 } from 'react-bootstrap';
 import './SimilarHighSchool.scss';
 import highSchoolAPI from '../../services/api/highSchool';
@@ -16,7 +16,7 @@ const SimilarHighSchool = () => {
     const [highSchools, setHighSchools] = useState([]);
     const [errorAlert, setErrorAlert] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
-
+    const [showSpinner, setShowSpinner] = useState(false);
     /**
      * Fetches student and high school data
      */
@@ -31,7 +31,9 @@ const SimilarHighSchool = () => {
                 setStudent(result.student);
             }
         });
+        setShowSpinner(true);
         highSchoolAPI.findSimilarHS(localStorage.getItem('username')).then((result) => {
+            setShowSpinner(false);
             if (result.error) {
                 setErrorAlert(true);
                 setErrorMessage(result.error);
@@ -123,77 +125,83 @@ const SimilarHighSchool = () => {
                                 <h1>Similar High Schools</h1>
                                 {errorAlert
                                     ? <Alert variant="danger">{errorMessage}</Alert>
-                                    : highSchools.map((highSchool) => (
-                                        <div className="border rounded-lg mb-2 p-3" key={highSchool.HighSchoolId}>
-                                            <Row className="mb-2">
-                                                <Col>
-                                                    <Row className="h5 font-weight-bold mb-0"><Col>{highSchool.Name}</Col></Row>
-                                                    <Row>
-                                                        <Col>{`${highSchool.City}, ${highSchool.State}`}</Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col className="h4 text-center">
-                                                    {highSchool.similarityPoints ? `${Math.round((highSchool.similarityPoints / 55) * 100)}%` : 'N/A'}
-                                                    {' Similarity'}
-                                                </Col>
-                                                <Col className="text-right">
-                                                    <Row>
-                                                        <Col><span className="niche-academic-score font-weight-bold text-center h3">{highSchool.NicheAcademicScore}</span></Col>
-                                                    </Row>
-                                                </Col>
-                                            </Row>
-                                            <Row>
-                                                <Col className="text-center">
-                                                    <div className="overview-title">Average SAT</div>
-                                                    <div className="similar-text">{highSchool.AverageSAT ? highSchool.AverageSAT : 'N/A'}</div>
-                                                </Col>
-                                                <Col>
-                                                    <Row>
-                                                        <Col>Math</Col>
-                                                        <Col>{highSchool.SATMath ? highSchool.SATMath : '-'}</Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <Col>EBRW</Col>
-                                                        <Col>{highSchool.SATEBRW ? highSchool.SATEBRW : '-'}</Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col className="text-center">
-                                                    <div className="overview-title">Average ACT</div>
-                                                    <div className="similar-text">{highSchool.AverageACT ? highSchool.AverageACT : 'N/A'}</div>
-                                                </Col>
-                                                <Col>
-                                                    <Row>
-                                                        <Col>Math</Col>
-                                                        <Col>{highSchool.ACTMath ? highSchool.ACTMath : '-'}</Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <Col>Reading</Col>
-                                                        <Col>{highSchool.ACTReading ? highSchool.ACTReading : '-'}</Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <Col>English</Col>
-                                                        <Col>{highSchool.ACTEnglish ? highSchool.ACTEnglish : '-'}</Col>
-                                                    </Row>
-                                                    <Row>
-                                                        <Col>Science</Col>
-                                                        <Col>{highSchool.ACTScience ? highSchool.ACTScience : '-'}</Col>
-                                                    </Row>
-                                                </Col>
-                                                <Col className="text-center">
-                                                    <div className="overview-title">Graduation Rate</div>
-                                                    <div className="similar-text">
-                                                        {highSchool.GraduationRate ? `${highSchool.GraduationRate}%` : 'N/A'}
-                                                    </div>
-                                                </Col>
-                                                <Col className="text-center">
-                                                    <div className="overview-title">Average GPA</div>
-                                                    <div className="similar-text">
-                                                        {highSchool.averageGPA ? highSchool.averageGPA.toFixed(2) : 'N/A'}
-                                                    </div>
-                                                </Col>
-                                            </Row>
-                                        </div>
-                                    ))}
+                                    : showSpinner
+                                        ? (
+                                            <div className="text-center">
+                                                <Spinner className="large-spinner" style={{ width: '10rem', height: '10rem' }} animation="grow" variant="primary" />
+                                            </div>
+                                        )
+                                        : highSchools.map((highSchool) => (
+                                            <div className="border rounded-lg mb-2 p-3" key={highSchool.HighSchoolId}>
+                                                <Row className="mb-2">
+                                                    <Col>
+                                                        <Row className="h5 font-weight-bold mb-0"><Col>{highSchool.Name}</Col></Row>
+                                                        <Row>
+                                                            <Col>{`${highSchool.City}, ${highSchool.State}`}</Col>
+                                                        </Row>
+                                                    </Col>
+                                                    <Col className="h4 text-center">
+                                                        {highSchool.similarityPoints ? `${Math.round((highSchool.similarityPoints / 55) * 100)}%` : 'N/A'}
+                                                        {' Similarity'}
+                                                    </Col>
+                                                    <Col className="text-right">
+                                                        <Row>
+                                                            <Col><span className="niche-academic-score font-weight-bold text-center h3">{highSchool.NicheAcademicScore}</span></Col>
+                                                        </Row>
+                                                    </Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col className="text-center">
+                                                        <div className="overview-title">Average SAT</div>
+                                                        <div className="similar-text">{highSchool.AverageSAT ? highSchool.AverageSAT : 'N/A'}</div>
+                                                    </Col>
+                                                    <Col>
+                                                        <Row>
+                                                            <Col>Math</Col>
+                                                            <Col>{highSchool.SATMath ? highSchool.SATMath : '-'}</Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col>EBRW</Col>
+                                                            <Col>{highSchool.SATEBRW ? highSchool.SATEBRW : '-'}</Col>
+                                                        </Row>
+                                                    </Col>
+                                                    <Col className="text-center">
+                                                        <div className="overview-title">Average ACT</div>
+                                                        <div className="similar-text">{highSchool.AverageACT ? highSchool.AverageACT : 'N/A'}</div>
+                                                    </Col>
+                                                    <Col>
+                                                        <Row>
+                                                            <Col>Math</Col>
+                                                            <Col>{highSchool.ACTMath ? highSchool.ACTMath : '-'}</Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col>Reading</Col>
+                                                            <Col>{highSchool.ACTReading ? highSchool.ACTReading : '-'}</Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col>English</Col>
+                                                            <Col>{highSchool.ACTEnglish ? highSchool.ACTEnglish : '-'}</Col>
+                                                        </Row>
+                                                        <Row>
+                                                            <Col>Science</Col>
+                                                            <Col>{highSchool.ACTScience ? highSchool.ACTScience : '-'}</Col>
+                                                        </Row>
+                                                    </Col>
+                                                    <Col className="text-center">
+                                                        <div className="overview-title">Graduation Rate</div>
+                                                        <div className="similar-text">
+                                                            {highSchool.GraduationRate ? `${highSchool.GraduationRate}%` : 'N/A'}
+                                                        </div>
+                                                    </Col>
+                                                    <Col className="text-center">
+                                                        <div className="overview-title">Average GPA</div>
+                                                        <div className="similar-text">
+                                                            {highSchool.averageGPA ? highSchool.averageGPA.toFixed(2) : 'N/A'}
+                                                        </div>
+                                                    </Col>
+                                                </Row>
+                                            </div>
+                                        ))}
                             </Container>
                         </>
                     ) : <Alert variant="info">Go to your profile and add a high school to see similar high schools.</Alert>}
